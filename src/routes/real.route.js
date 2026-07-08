97f0d92e3814c1adb5d38ff7ff9f8658f6ace4d6
AhmadAmin02 <ahmadamin0007404@gmail.com>
2026-07-09 02:10:28 +0800

    update real.route.js

--- src/routes/real.route.js
+++ src/routes/real.route.js
@@ -19,7 +19,9 @@ router.get("/", async (req, res, next) => {
     const link = `https://amprem.irfanjawa.com/`;
     await page.goto(link, { waitUntil: "domcontentloaded", timeout: 60000 });
     
-    await new Promise(resolve => setTimeout(resolve, 100000)); // 1 detik
+    //if (page.url()) 
+    
+    //await new Promise(resolve => setTimeout(resolve, 100000)); // 1 detik
     const html = await page.content();
     
     
@@ -28,7 +30,7 @@ router.get("/", async (req, res, next) => {
     
     res.json({
       link,
-      html,
+      page: page.url(),
       screenshot
     });
   } catch (err) {
