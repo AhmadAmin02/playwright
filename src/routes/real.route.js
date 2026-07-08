2768dde20a647a2ebb1c8ca6cda9538c6261c30b
AhmadAmin02 <ahmadamin0007404@gmail.com>
2026-07-09 04:03:38 +0800

    update real.route.js

--- src/routes/real.route.js
+++ src/routes/real.route.js
@@ -37,7 +37,7 @@ router.get("/", async (req, res, next) => {
       return el;
     }, { timeout: 60000 });*/
     
-    await new Promise(resolve => setTimeout(resolve, 15000)); // 1 detik
+    await new Promise(resolve => setTimeout(resolve, 20000)); // 1 detik
     await page.evaluate(() => {
       const setValue = (el, value) => {
         const setter = Object.getOwnPropertyDescriptor(
@@ -53,7 +53,7 @@ router.get("/", async (req, res, next) => {
       
       document.querySelector('button[type="submit"]').click();
     });
-    await new Promise(resolve => setTimeout(resolve, 3000));
+    await new Promise(resolve => setTimeout(resolve, 5000));
     const html = await page.content();
     const result = await page.evaluate(async () => {
       const res = await fetch("https://amprem.irfanjawa.com/api/auth/send-magic-link", {
