8091fd0451af0af158a28ad06ab560f69ad9dbbe
AhmadAmin02 <ahmadamin0007404@gmail.com>
2026-07-18 20:30:55 +0800

    update cek.route.js

--- src/routes/cek.route.js
+++ src/routes/cek.route.js
@@ -25,9 +25,9 @@ router.get("/data", (req, res) => {
   res.json({ status: "ok", data });
 });
 
-const acc1 = JSON.parse(fs.readFileSync("../lib/accounts.json", "utf8"));
+const acc1 = JSON.parse(fs.readFileSync(path.join(__dirname, "../lib/accounts.json"), "utf8"));
 login.main(1, "01/01/2006", "31/12/2008", acc1);
-const acc2 = JSON.parse(fs.readFileSync("../lib/acc2.json", "utf8"));
+const acc2 = JSON.parse(fs.readFileSync(path.join(__dirname, "../lib/acc2.json"), "utf8"));
 login.main(2, "01/01/2006", "31/12/2008", acc2);
 
 
